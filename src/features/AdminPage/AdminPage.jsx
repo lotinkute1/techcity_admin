aa19b18e3e5855f8d933e3d5f637a4eb23da0852
Phuc Hoang <phuchoang1232000@gmail.com>
2022-05-15 11:54:11 +0700

    add column chart

--- src/features/AdminPage/AdminPage.jsx
+++ src/features/AdminPage/AdminPage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ColumnChart from "../../components/Chart/Column";
 import LineChart from "../../components/Chart/Line";
 
 export default function AdminPage() {
@@ -38,6 +39,40 @@ export default function AdminPage() {
     {
       year: "1999",
       value: 13,
+    },
+  ];
+  const data2 = [
+    {
+      type: '家具家电',
+      sales: 38,
+    },
+    {
+      type: '粮油副食',
+      sales: 52,
+    },
+    {
+      type: '生鲜水果',
+      sales: 61,
+    },
+    {
+      type: '美容洗护',
+      sales: 145,
+    },
+    {
+      type: '母婴用品',
+      sales: 48,
+    },
+    {
+      type: '进口食品',
+      sales: 38,
+    },
+    {
+      type: '食品饮料',
+      sales: 38,
+    },
+    {
+      type: '家庭清洁',
+      sales: 38,
     },
   ];
   return (
@@ -156,6 +191,7 @@ export default function AdminPage() {
             </div>
             <div className="card-body">
               <LineChart data={data} />
+              <ColumnChart data={data2}/>
             </div>
           </div>
         </div>
